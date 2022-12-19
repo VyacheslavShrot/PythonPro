7234b1e7353c84faa2d03ba15b47a411f7fe913b
VyacheslavShrot <slavic.shrot@gmail.com>
2022-12-20 00:49:20 +0200

    hw6 done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 import random
 import string
 import pandas as pd
@@ -20,6 +21,8 @@ from flask import Flask, Response
 from webargs import fields, validate
 
 from webargs.flaskparser import use_kwargs
+
+from colorama import Fore, Back, Style
 
 
 app = Flask(__name__)
@@ -330,14 +333,14 @@ class Point:
         self.y = y
 
 
-class Circle():
+class Circle:
 
     def __init__(self, circle_x, circle_y, radius):
         self.circle_x = circle_x
         self.circle_y = circle_y
         self.radius = radius
 
-    def contains(self, Point):
+    def __contains__(self, Point):
 
         if (
                 (Point.x - self.circle_x) * (Point.x - self.circle_x) +
@@ -348,4 +351,162 @@ class Circle():
         else:
             return False
 
-app.run(port=5001, debug=True)
+
+class frange:
+
+    def frange(start, stop=None, step=None):
+        start = float(start)
+
+        if stop is None:
+            stop = start + 0.0
+            start = 0.0
+
+        if step is None:
+            step = 1.0
+
+        count = 0
+        while True:
+            temp = float(start + count * step)
+
+            if step > 0 and temp >= stop:
+                break
+
+            elif step < 0 and temp <= stop:
+                break
+            yield temp
+
+            count += 1
+
+
+class colorizer:
+
+    def __init__(self, text_color):
+        self.text_color = text_color
+
+    def __enter__(self):
+        self.color_blue = Fore.BLUE
+        self.color_green = Fore.GREEN
+        self.color_red = Fore.RED
+
+        if self.text_color == 'red':
+            return self.color_red + self.text_color
+
+        elif self.text_color == 'blue':
+            return self.color_blue + self.text_color
+
+        elif self.text_color == 'green':
+            return self.color_green + self.text_color
+
+        else:
+            return f'choose another color or check your input color'.upper()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(Style.RESET_ALL)
+        return print('printed in default color')
+
+
+with colorizer('red') as c:
+    print(c)
+
+
+class Shape:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def square(self):
+        return 0
+
+
+class Circle(Shape):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def square(self):
+        return math.pi * self.radius ** 2
+
+
+class Rectangle(Shape):
+
+    def __init__(self, x, y, height, width):
+        super().__init__(x, y)
+        self.height = height
+        self.width = width
+
+    def square(self):
+        return self.width * self.height
+
+
+class Parallelogram(Rectangle):
+
+    def __init__(self, x, y, height, width, angle):
+        super().__init__(x, y, height, width)
+        self.angle = angle
+
+    def print_angle(self):
+        print(self.angle)
+
+    def __str__(self):
+        result = super().__str__()
+        return result + f'\nParallelogram: {self.width}, {self.height}, {self.angle}'
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def square(self):
+        return self.width * self.height
+
+
+class Triangle(Parallelogram):
+
+    def __init__(self, x, y, height, width, angle):
+        super().__init__(x, y, height, width, angle)
+
+    def square(self):
+        return (0.5 * self.width) * self.height
+
+
+class Scene:
+    def __init__(self):
+        self._figures = []
+
+    def add_figure(self, figure):
+        self._figures.append(figure)
+
+    def total_square(self):
+        return sum(f.square() for f in self._figures)
+
+    def __str__(self):
+        pass
+
+
+r = Rectangle(0, 0, 10, 20)
+r1 = Rectangle(10, 0, -10, 20)
+r2 = Rectangle(0, 20, 100, 20)
+
+c = Circle(10, 0, 10)
+c1 = Circle(100, 100, 5)
+
+p = Parallelogram(1, 2, 20, 30, 45)
+p.x
+p1 = Parallelogram(1, 2, 20, 30, 45)
+str(p1)
+
+scene = Scene()
+scene.add_figure(r)
+scene.add_figure(r1)
+scene.add_figure(r2)
+scene.add_figure(c)
+scene.add_figure(c1)
+
+scene.total_square()
+
+
+
+
+
+
+# app.run(port=5001, debug=True)
